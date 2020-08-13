4282513743ec40f9c58fe8b59219692d7c72fcd8
s0i37 <s0i37@work>
2020-08-13 16:38:20 +0500

    added date logging

--- README.md
+++ README.md
@@ -8,11 +8,21 @@ append in /etc/bind/db.yourzone.tk your dns server IP:
 
 `*	IN	A	1.2.3.4`
 
+### creating certificate
+
+`openssl genrsa -out keys.pem 2048`
+
+`openssl req -new -x509 -key keys.pem -out cert.pem`
+
+### tor (optional)
+
+`tor`
+
 ### running
 
 `node index.js`
 
 ### usage
 
-just open any URL like www.somesite.org.yourzone.tk
+just open any URL like http://www.somesite.org.yourzone.tk or http://somethingsite.onion.yourzone.tk
 
--- proxy.js
+++ proxy.js
@@ -28,12 +28,14 @@ function replace_body(buffer, host)
 function proxy(proto, port, request, response)
 {
 	var { method, headers, url } = request,
-		chunks = []
+		chunks = [],
+		now = new Date(),
+		time = `${now.getDate()}.${now.getMonth()+1}.${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
 
 	if(headers.host.indexOf(DNS_SUFFIX) == -1)
-		throw '[!] outside request: ' + request.connection.remoteAddress +  ' - ' + headers.host
+		throw '[!] [' + time + ']: ' + request.connection.remoteAddress +  ' - ' + headers.host
 	else
-		console.log('[+] request: ' + request.connection.remoteAddress +  ' - ' + headers.host)
+		console.log('[+] [' + time + ']: ' + request.connection.remoteAddress +  ' - ' + headers.host)
 	headers.host = headers.host.replace(DNS_SUFFIX, '')
 	agent = (headers.host.split('.').pop().toLowerCase() == 'onion') ? tor : null
 
